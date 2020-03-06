c7c71f7fd6a74e68b618323566a967fe7a9a25c7
Kris Foster <krisfoster@live.ie>
2020-03-06 18:35:07 +0000

    cards padding

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -11,7 +11,8 @@ const useStyles = makeStyles(theme => ({
     marginTop: theme.spacing(2),
     marginBottom: theme.spacing(2),
     textAlign: "center",
-    color: theme.palette.text.secondary
+    color: theme.palette.text.secondary,
+    backgroundColor: theme.palette.background.default
   }
 }));
 
--- src/components/StoryBoard.jsx
+++ src/components/StoryBoard.jsx
@@ -4,6 +4,7 @@ import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 import Divider from "@material-ui/core/Divider";
+import Paper from "@material-ui/core/Paper";
 import Story from "./Story";
 import NewStoryModal from "./NewStoryModal"
 import NewStageModal from "./NewStageModal"
@@ -13,8 +14,10 @@ const styles = theme => ({
     display: "flex",
     marginBottom: theme.spacing(2)
   },
-  stageContainer: {
-    marginTop: theme.spacing(2),
+  paper: {
+    marginTop: theme.spacing(4),
+    padding: theme.spacing(4),
+    backgroundColor: theme.palette.background.paper
   },
   stageTitle: {
     textAlign: "center",
@@ -42,18 +45,22 @@ class StoryBoard extends React.Component {
         </Box>
         <Divider />
         <Grid container spacing={3}>
-          {storiesByStage.map((stageWithStories, i) => {
-            return (
-              <Grid key={i} item xs={4} className={classes.stageContainer}>
-                <Typography variant="h6" gutterBottom className={classes.stageTitle}>
-                  {stages[i].name}
-                  </Typography>
-                {stageWithStories.map((story, id) => (
-                  <Story key={id} story={story} stages={stages} />
-                ))}
-              </Grid>
-            )
-          })}
+          <Grid container justify="center" spacing={3}>
+            {storiesByStage.map((stageWithStories, i) => {
+              return (
+                <Grid key={i} item xs={3}>
+                  <Paper className={classes.paper}>
+                    <Typography variant="h6" gutterBottom className={classes.stageTitle}>
+                      {stages[i].name}
+                    </Typography>
+                    {stageWithStories.map((story, id) => (
+                      <Story key={id} story={story} stages={stages} />
+                    ))}
+                  </Paper>
+                </Grid>
+              )
+            })}
+          </Grid>
         </Grid>
       </div>
     );
